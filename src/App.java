d76b5133fa5afca3dae2307b7194b39a0363156d
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-10-11 15:11:31 +0200

    10_11

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
+    static int sum(int ... ret){
+        int s =0;
+        for (int i : ret) {
+            s+=i;
+        }
+        return s;
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(sum(7,6));
+        int [] d = {7,5};
+        System.out.println(sum(d));
+        
     }
 }
--- src/Praktikum_10_11_2.java
+++ src/Praktikum_10_11_2.java
@@ -1,4 +1,4 @@
-double safeSum(double [] ar){
+double safeSum(double ... ar){
     double sum = 0;
     for(int i=0; i < ar.length; i++) {
         sum += ar[i];
@@ -6,4 +6,3 @@ double safeSum(double [] ar){
     return sum;
 }
 
-double [] t = {7,77,8};
