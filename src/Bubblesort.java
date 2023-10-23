e6453881df80bb5ccfeed6d62b2cb91e5c9f6e4b
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-10-23 16:03:22 +0200

    lol

---
+++ src/Bubblesort.java
@@ -0,0 +1,23 @@
+void swap(int[] nums, int i, int j) {
+    assert i >= 0 && i < nums.length;
+    assert j >= 0 && j < nums.length;
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp; 
+}
+
+boolean bubble(int[] numbers) {
+    boolean swapped = false;
+    for (int i = 0; i < numbers.length - 1; i++) {
+        if (numbers[i] > numbers[i + 1]) {
+            swap(numbers, i, i + 1);
+            swapped = true;
+        }
+    }
+    return swapped;
+}
+
+int[] bubblesort(int... numbers) {
+    while (bubble(numbers));
+    return numbers;
+}
---
+++ src/Praktikum_10_18.java
@@ -0,0 +1,25 @@
+return new Random().ints(3,'A','Z'+1).mapToObj(i ->(char)i).toArray()
+
+void swap(int[] nums, int i, int j) {
+    assert i >= 0 && i < nums.length;
+    assert j >= 0 && j < nums.length;
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp; 
+}
+
+boolean bubble(int[] numbers) {
+    boolean swapped = false;
+    for (int i = 0; i < numbers.length - 1; i++) {
+        if (numbers[i] > numbers[i + 1]) {
+            swap(numbers, i, i + 1);
+            swapped = true;
+        }
+    }
+    return swapped;
+}
+
+int[] bubblesort(int... numbers) {
+    while (bubble(numbers));
+    return numbers;
+}
